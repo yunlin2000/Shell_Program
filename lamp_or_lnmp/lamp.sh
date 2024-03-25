a12549db1bdfd663205ca801e3da877c4704bfbd
Ken.Zhao <yunlin2000@163.com>
2024-03-25 22:40:54 +0800

    安装lamp或lnmp的Linux Shell scripts，第一次提交

---
+++ lamp_or_lnmp/lamp.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+echo "Lamp is installed."
+
---
+++ lamp_or_lnmp/lamp_or_lnmp.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# 显示菜单
+cat << END
+1.[Install Lamp]
+2.[Install Lnmp]
+3.[Exit]
+END
+# 接受用户选择
+read -p "Pls select a number[1|2|3]:"
+
+
